729f5e0f6c2ba44e3b870a249e9911e486d3aad4
korshikov <pk.95@mail.ru>
2015-03-23 17:45:02 +0300

    lenwords

--- Makefile
+++ Makefile
@@ -2,10 +2,16 @@ all:
 	make -C lib/
 	make -C cat/
 	make -C revwords/
+<<<<<<< HEAD
 	make -C lenwords/
+=======
+>>>>>>> c6440ed... lenwords
 
 clean:
 	make clean -C lib/
 	make clean -C cat/
 	make clean -C revwords/
+<<<<<<< HEAD
 	make clean -C lenwords/
+=======
+>>>>>>> c6440ed... lenwords
--- lenwords/lenwords.c
+++ lenwords/lenwords.c
@@ -4,13 +4,32 @@
 #include <stdio.h>
 #include <helpers.h>
 
+<<<<<<< HEAD
 const size_t BUFFER_SIZE = 4097;
+=======
+const size_t BUFFER_SIZE = 4096;
+>>>>>>> c6440ed... lenwords
 
 void print_err() {
     char *msg = strerror(errno);
     write_(STDERR_FILENO, msg, strlen(msg));
 }
 
+<<<<<<< HEAD
+=======
+void reverse(char *begin, char *end) {
+    char temp;
+    while(begin<end)
+    {
+        temp = *end;
+        *(end--) = *begin;
+        *(begin++) = temp;
+
+    }
+    return;
+}
+
+>>>>>>> c6440ed... lenwords
 int main() {
     char buffer[BUFFER_SIZE];
     int n;
@@ -20,7 +39,11 @@ int main() {
             print_err();
         }
 
+<<<<<<< HEAD
         n = sprintf(buffer,"%d ",(n - (buffer[n - 1] == ' ')));
+=======
+        buffer = sprintf("%d ",(buffer + n - 2 + (buffer[n - 1] != ' ')-buffer));
+>>>>>>> c6440ed... lenwords
         if (write(STDOUT_FILENO, buffer, n) == -1) {
             print_err();
         }
